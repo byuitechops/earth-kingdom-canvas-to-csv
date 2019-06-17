1fde8fae6f426f7ba07fa7391cf11a545b9bfec7
KayleeHartzog <har15031@byui.edu>
2019-06-17 14:09:24 -0600

    did some changes in processor.

--- CsvFileProcessor.cs
+++ CsvFileProcessor.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using CsvHelper;
 using System.Collections.Generic;
+using System.IO.TextWriter;
 
 namespace first_C__project
 {
@@ -10,11 +11,27 @@ namespace first_C__project
     {
          public static string CsvFileProcessor(dynamic json)
         {
-            // using ()
-            // WriteDynamicHeader(json);
-            // WriteRecords()
+            // using (TextWriter writer = new StreamWriter(@"c:\test.csv", false, System.Text.Encoding.UTF8))
+            // {
+            //     var csv = new CsvWriter(writer);
+            //     csv.WriteRecords(values);
+            // }
 
-            // Console.WriteLine(json);
+            // var filename = 'activity.csv';
+            // TextWriter writer = File.CreateText(@"\test.csv");
+            // var csv = new CsvWriter(writer);
+            // csv.Configuration.Encoding = Encoding.UTF8;
+            // foreach (var value in json)
+            // {
+            //     csv.WriteRecords(value);
+            // }
+            // writer.Close();
+
+            using (var writer = new StreamWriter(Directory.GetCurrentDirectory() + @"\activity01.csv"));
+            using (var csv = new CsvWriter(writer))
+            {
+
+            }
 
             return "0";
            
