b03c469d1c7327c910e7330a66899a93c2c4374a
Seth Bolander <bol15015@byui.edu>
2019-06-19 14:57:32 -0600

    removed append=true in FileProcessor.cs

--- CsvFileProcessor.cs
+++ CsvFileProcessor.cs
@@ -12,23 +12,7 @@ namespace first_C__project
     {
         public static string CsvFileProcessor(JArray json)
         {
-            // using (TextWriter writer = new StreamWriter(@"c:\test.csv", false, System.Text.Encoding.UTF8))
-            // {
-            //     var csv = new CsvWriter(writer);
-            //     csv.WriteRecords(values);
-            // }
-
-            // var filename = 'activity.csv';
-            // TextWriter writer = File.CreateText(@"\test.csv");
-            // var csv = new CsvWriter(writer);
-            // csv.Configuration.Encoding = Encoding.UTF8;
-            // foreach (var value in json)
-            // {
-            //     csv.WriteRecords(value);
-            // }
-            // writer.Close();
-
-            using (TextWriter writer = new StreamWriter(Directory.GetCurrentDirectory() + @"\activity01.csv", true, System.Text.Encoding.UTF8))
+            using (TextWriter writer = new StreamWriter(Directory.GetCurrentDirectory() + @"\activity01.csv", false, System.Text.Encoding.UTF8))
             {
                 var csv = new CsvWriter(writer);
                 var firstObject = json[0];
